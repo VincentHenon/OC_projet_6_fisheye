19dbc95b5e3db66f4aaf391247f4c26465967439
Vincent Henon <mr.henonvincent@gmail.com>
2023-04-12 02:52:20 +0200

    page photographe. A faire: modal + lightbox

--- Front-End-Fisheye-main/scripts/factories/photographer.js
+++ Front-End-Fisheye-main/scripts/factories/photographer.js
@@ -1,17 +1,56 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price, id } = data;
+  
     const picture = `assets/photographers/${portrait}`;
-
+  
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+      //create unique Url for each photographer
+      const photographerUrl = `photographer.html?id=${id}`
+
+      // Create HTML tags
+      const articleEl = document.createElement("article");
+      const linkEl = document.createElement("a");
+      const profileEl = document.createElement("div")
+      const img = document.createElement("img");
+      const infosEl = document.createElement("div");
+      const locationEl = document.createElement("p");
+      const taglineEl = document.createElement("p");
+      const priceEl = document.createElement("p");
+      const nameEl = document.createElement("h2");
+      
+      // Set attributes
+      linkEl.setAttribute("href", photographerUrl); //link to photographer
+      img.setAttribute("src", picture);
+      img.setAttribute("alt", `picture of ${name}`);
+
+      // positionning elements inside the first part of the card
+      articleEl.appendChild(profileEl);
+      profileEl.appendChild(linkEl);
+      linkEl.appendChild(img);
+      linkEl.appendChild(nameEl);
+      
+      // Create Classes
+      profileEl.classList.add("profile");
+      img.classList.add("profileImg");
+      infosEl.classList.add("infos");
+      locationEl.classList.add("infos-location");
+      taglineEl.classList.add("infos-tagline")
+      priceEl.classList.add("infos-price");
+  
+      // Add text
+      locationEl.textContent = `${city}, ${country}`;
+      taglineEl.textContent = `"${tagline}"`;
+      priceEl.textContent = `${price}€/jour`;
+      nameEl.textContent = name;
+
+      // Positionning elements inside the lower part of the card
+      infosEl.appendChild(locationEl);
+      infosEl.appendChild(taglineEl);
+      infosEl.appendChild(priceEl);
+      articleEl.appendChild(infosEl);
+
+      return articleEl;
     }
-    return { name, picture, getUserCardDOM }
-}
+  
+    return { name, picture, getUserCardDOM };
+  }
--- Front-End-Fisheye-main/scripts/pages/index.js
+++ Front-End-Fisheye-main/scripts/pages/index.js
@@ -1,7 +1,36 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
+// Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+// mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+async function getPhotographers() {
+  try {
+    const response = await fetch("./data/photographers.json");
+    const photographers = await response.json();
+    console.log(photographers);
+    return photographers;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  console.log( photographers );
+  displayData(photographers);
+}
+
+init();
+
+/*let photographers = [
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -24,23 +53,6 @@
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
-    }
+    }*/
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+//export const { photographers };
